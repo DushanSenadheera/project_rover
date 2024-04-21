94dbea8247eaf71f5e46b289a5d83f665cb5acc3
Dushan Senadheera <dcsenadheera777@gmail.com>
2024-04-22 05:10:48 +0530

    add loading when data fetching

--- client/src/pages/Results/Results.tsx
+++ client/src/pages/Results/Results.tsx
@@ -1,9 +1,35 @@
-import './Results.scss';
+import "./Results.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Loading from "../../components/Loading/Loading";
 
-export default function Results(){
-    return(
-        <div>
-            <h1>Results</h1>
+export default function Results() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/location")
+      .then(function (res) {
+        const locations = res.data.location;
+        console.log(locations);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <Loading />; 
+  } else {
+    return (
+      <div>
+        <div className="results-content">
+          <h1>Results</h1>
+          <p>Here are the results of your search...</p>
         </div>
-    )
+      </div>
+    );
+  }
 }
