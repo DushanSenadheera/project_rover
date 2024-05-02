c9d5d0c65b16b8932bc854cd3b10a8dba31e533c
Dushan Senadheera <dcsenadheera777@gmail.com>
2024-05-02 16:17:36 +0530

    connect client and server

--- client/src/components/Card/Cards.tsx
+++ client/src/components/Card/Cards.tsx
@@ -1,4 +1,6 @@
 import { Image, Card, Text, Group, Button, rem } from "@mantine/core";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Carousel } from "@mantine/carousel";
 import { IconStar } from "@tabler/icons-react";
 import classes from "./Card.module.scss";
@@ -11,7 +13,26 @@ const images = [
   "https://images.unsplash.com/photo-1616486029423-aaa4789e8c9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
 ];
 
-export default function Cards() {
+export default function Cards(props) {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:8000/api/stay", {
+        location: props.location,
+      })
+      .then(function (res) {
+        setData(res.data.accomadations);
+        console.log(res.data.accomadations);
+        // setLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+        // setLoading(false);
+      });
+  }, []);
+
   const slides = images.map((image) => (
     <Carousel.Slide key={image}>
       <Image src={image} height={220} />
@@ -19,7 +40,8 @@ export default function Cards() {
   ));
 
   return (
-    <Card w={350} radius="md" withBorder padding="xl">
+    data.slice(-3).map((item) => (
+      <Card w={350} radius="md" withBorder padding="xl">
       <Card.Section>
         <Carousel
           withIndicators
@@ -36,27 +58,25 @@ export default function Cards() {
 
       <Group justify="space-between" mt="lg">
         <Text fw={500} fz="lg">
-          Forde, Norway
+          {item.Name}
         </Text>
 
         <Group gap={5}>
           <IconStar style={{ width: rem(16), height: rem(16) }} />
           <Text fz="xs" fw={500}>
-            4.78
+            {item.Rating}
           </Text>
         </Group>
       </Group>
 
       <Text fz="sm" c="dimmed" mt="sm">
-        Relax, rejuvenate and unplug in this unique contemporary Birdbox. Feel
-        close to nature in ultimate comfort. Enjoy the view of the epic mountain
-        range of Blegja and the Førdefjord.
+        {item.Location}
       </Text>
 
       <Group justify="space-between" mt="md">
         <div>
           <Text fz="xl" span fw={500} className={classes.price}>
-            397$
+            USD {item.Price}
           </Text>
           <Text span fz="sm" c="dimmed">
             {" "}
@@ -67,5 +87,7 @@ export default function Cards() {
         <Button radius="md">Book now</Button>
       </Group>
     </Card>
+    ))
+      
   );
 }
